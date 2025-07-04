6f2373de656b83bf41b0b9fc9e88b8160a1fcfcd
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-07-05 00:11:18 +0300

    5

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,25 @@
+import { useState } from 'react';
+
 function Home() {
+   const [count, setCount] = useState(0);
+
    return (
       <div style={{ padding: '100px 400px' }}>
-         <p style={{ color: 'white', fontSize: '100px', marginBottom: '20px' }}>1</p>
-         <button style={{ fontSize: '50px', width: '100px' }}>+</button>
-         <button style={{ fontSize: '50px', width: '100px' }}>-</button>
+         <p style={{ color: 'white', fontSize: '100px', marginBottom: '20px' }}>
+            {count}
+         </p>
+         <button
+            onClick={() => setCount((current) => current + 1)}
+            style={{ fontSize: '50px', width: '100px' }}
+         >
+            +
+         </button>
+         <button
+            onClick={() => setCount((current) => Math.max(current - 1, 0))}
+            style={{ fontSize: '50px', width: '100px' }}
+         >
+            -
+         </button>
       </div>
    );
 }
