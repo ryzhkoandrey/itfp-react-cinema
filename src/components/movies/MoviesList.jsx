0ffb2ff46bc208467533e8b8cf9ff3f68a4223b7
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-07-04 18:21:06 +0300

    4

--- src/components/movies/MoviesList.jsx
+++ src/components/movies/MoviesList.jsx
@@ -1,12 +1,10 @@
 import MoviesCard from './MoviesCard';
 
 function MoviesList() {
+   const arrayFilms = ['Kenguru', 'Dog', 'Joker', 'Betman', 'Superman'];
+
    return (
       <div id="movies" className="movies anchor">
-         <MoviesCard />
-         <MoviesCard />
-         <MoviesCard />
-         <MoviesCard />
          <MoviesCard />
          <MoviesCard />
          <MoviesCard />
