c2046216e95a8c848cf4a0fabf7ade5ff56e6116
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-07-05 15:28:11 +0300

    6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css';
-import { useEffect, useState } from 'react';
 import Header from './components/layout/Header';
 import Banner from './components/home/Banner';
 import Filter from './components/home/Filter';
@@ -7,28 +6,8 @@ import MoviesList from './components/movies/MoviesList';
 import Footer from './components/layout/Footer';
 
 function App() {
-   const [value, changeValue] = useState([]);
-
-   useEffect(() => {
-      fetch('https://jsonplaceholder.typicode.com/users')
-         .then((response) => {
-            if (!response.ok) {
-               throw new Error('Произошла ошибка');
-            } else {
-               return response.json();
-            }
-         })
-         .then((response) => {
-            console.log(response);
-            changeValue(response);
-         })
-         .catch((error) => console.error(error));
-   }, []);
-
    return (
       <>
-         <p style={{ color: '#fff' }}>{value[0]?.name}</p>
-
          <Header />
          <Banner />
 
