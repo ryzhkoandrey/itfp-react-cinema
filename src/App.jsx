bf07933ebcc2ae1700d97e344bacc5f6f09a0e56
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-07-10 21:02:07 +0300

    9

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
-import './App.css';
+import { createContext, useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 import Home from './pages/Home';
 import Favorites from './pages/Favorites';
 import Movie from './pages/Movie';
 import NotFound from './pages/NotFound';
-import Footer from './components/layout/Footer';
+import './App.css';
+
+export const Films = createContext();
 
 function App() {
+   const [value, changeValue] = useState([]);
+
+   useEffect(() => {
+      async function fetchItems() {
+         await fetch('https://686fd2504838f58d1123034b.mockapi.io/movies')
+            .then((data) => data.json())
+            .then((data) => changeValue(data))
+            .catch((error) => console.error(error));
+      }
+      fetchItems();
+   }, []);
+
    return (
       <>
          <BrowserRouter>
             <Header />
-
-            <Routes>
-               <Route path="/" element={<Home />} />
-               <Route path="/favorites" element={<Favorites />} />
-               <Route path="/movie" element={<Movie />} />
-               <Route path="*" element={<NotFound />} />
-            </Routes>
-
+            <Films.Provider value={value}>
+               <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/favorites" element={<Favorites />} />
+                  <Route path="/movie" element={<Movie />} />
+                  <Route path="*" element={<NotFound />} />
+               </Routes>
+            </Films.Provider>
             <Footer />
          </BrowserRouter>
       </>
--- src/components/movies/MoviesList.jsx
+++ src/components/movies/MoviesList.jsx
@@ -1,19 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useContext } from 'react';
 import MoviesCard from './MoviesCard';
+import { Films } from '../../App';
 
 function MoviesList() {
-   const [movies, setMovies] = useState([]);
-
-   useEffect(() => {
-      fetch('https://686fd2504838f58d1123034b.mockapi.io/movies')
-         .then((data) => data.json())
-         .then((data) => setMovies(data))
-         .catch((error) => console.error(error));
-   }, []);
+   const films = useContext(Films);
+   console.log(films);
 
    return (
       <div id="movies" className="movies anchor">
-         {movies.map((movie) => (
+         {films.map((movie) => (
             <MoviesCard key={movie.id} />
          ))}
       </div>
