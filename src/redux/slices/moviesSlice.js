346b4860c7c8a4878df73027d460ad9cc7550be6
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-07-11 19:55:49 +0300

    11

--- src/redux/slices/moviesSlice.js
+++ src/redux/slices/moviesSlice.js
@@ -1,11 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-const initialState = [];
+const initialState = {
+   films: [],
+   status: null,
+   errors: null,
+};
+
+export const fetchMovies = createAsyncThunk('movie/fetchmovies', async () => {
+   try {
+      const movies = await fetch(
+         'https://686fd2504838f58d1123034b.mockapi.io/movies'
+      );
+      if (!movies.ok) throw new Error('Error on server, sorry');
+      return movies.json();
+   } catch (error) {
+      console.error(error);
+      throw error;
+   }
+});
 
 const moviesSlice = createSlice({
-   name: 'movies',
+   name: 'movie',
    initialState,
    reducers: {},
+
+   extraReducers: (builder) => {
+      builder.addCase(fetchMovies.pending, (state) => {
+         state.status = 'loading';
+      });
+   },
 });
 
 export default moviesSlice.reducer;
