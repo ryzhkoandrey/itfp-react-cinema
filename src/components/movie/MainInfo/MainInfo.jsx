f402406281f06859f4279108d426efb9aa062129
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-07-15 19:56:06 +0300

    14

--- src/components/movie/MainInfo/MainInfo.jsx
+++ src/components/movie/MainInfo/MainInfo.jsx
@@ -1,10 +1,23 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { searchFilmInState } from '../../../redux/slices/moviesSlice';
 import MainInfoLeft from './MainInfoLeft';
 import MainInfoRight from './MainInfoRight';
 
 function MainInfo() {
+   const id = useParams();
+   const dispatch = useDispatch();
+
+   useEffect(() => {
+      dispatch(searchFilmInState(id));
+   }, [dispatch]);
+
+   const { searchFilm } = useSelector((store) => store.movies);
+
    return (
       <div className="main__info">
-         <MainInfoLeft />
+         <MainInfoLeft filmInfo={{ ...searchFilm }} />
          <MainInfoRight />
       </div>
    );
--- src/components/movie/MainInfo/MainInfoLeft.jsx
+++ src/components/movie/MainInfo/MainInfoLeft.jsx
@@ -1,45 +1,24 @@
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { searchFilmInState } from '../../../redux/slices/moviesSlice';
 import Rating from '../Rating';
 
-function MainInfoLeft() {
-   const id = useParams();
-   const dispatch = useDispatch();
-
-   useEffect(() => {
-      dispatch(searchFilmInState(id));
-   }, [dispatch]);
-
-   const { searchFilm } = useSelector((store) => store.movies);
-
-   console.log(searchFilm);
+function MainInfoLeft(props) {
+   const dataFilm = props.filmInfo.film;
 
    return (
       <div className="main__info-left">
          <div className="main__info-left-title">
-            <p>{searchFilm.film.title}</p>
-            {/* <p>Бегущий по лезвию 2049</p> */}
+            <p>{dataFilm?.title}</p>
             <Rating />
          </div>
 
          <div className="main__info-left-categoryes">
-            <p>18+</p>
-            <p>HD</p>
-            <p>2022</p>
-            <p>Movie</p>
+            {dataFilm?.categoryes.map((value, index) => (
+               <p key={index}>{value}</p>
+            ))}
          </div>
 
          <div className="main__info-left-about">
             <p>Description</p>
-            <p>
-               Two years have passed unnoticed in Naruto's world. Former newcomers
-               joined the ranks of experienced Shinobi in the ranks of tuning and
-               zenin. The main characters did not sit still – everyone became a
-               disciple of one of the legendary Sannin – the three great ninja
-               Konoha.
-            </p>
+            <p>{dataFilm?.description}</p>
          </div>
       </div>
    );
