9a30a2ba3c449db33c86b4474b411ea0d7e48637
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-07-16 12:09:38 +0300

    15

--- src/components/movie/Video.jsx
+++ src/components/movie/Video.jsx
@@ -17,7 +17,6 @@ function Video() {
                   height="700"
                   src={`https://www.youtube.com/embed/${videoURL}`}
                   title="YouTube video player"
-                  frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
                ></iframe>
