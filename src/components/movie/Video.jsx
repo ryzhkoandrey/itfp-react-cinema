e31ba23ae615fa46e5cc8096be44972a97725a78
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-07-16 16:52:31 +0300

    15

--- src/components/movie/Video.jsx
+++ src/components/movie/Video.jsx
@@ -2,28 +2,27 @@ import { useSelector } from 'react-redux';
 import searchParamInYoutube from '../../tools/searchParamInYoutube';
 
 function Video() {
-   const { searchFilm } = useSelector((store) => store.movies);
-   // const trailerurl = searchFilm.film.trailerUrl;
-   // const videoURL = searchParamInYoutube(trailerurl);
+   const { searchFilm, status } = useSelector((store) => store.movies);
+
+   if (status === 'loading') {
+      return <p>Loading...</p>;
+   }
+
+   const trailerurl = searchFilm.film?.trailerUrl;
+   const videoURL = trailerurl ? searchParamInYoutube(trailerurl) : '';
 
    return (
-      <>
-         {searchFilm.status === 'loading' ? (
-            <p>Loading...</p>
-         ) : (
-            <div className="video-wrapper">
-               <iframe
-                  width="100%"
-                  height="700"
-                  src={`https://www.youtube.com/embed/${videoURL}`}
-                  title="YouTube video player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-               ></iframe>
-            </div>
-         )}
-      </>
+      <div className="video-wrapper">
+         <iframe
+            width="100%"
+            height="700"
+            src={`https://www.youtube.com/embed/${videoURL}`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+         ></iframe>
+      </div>
    );
 }
 
