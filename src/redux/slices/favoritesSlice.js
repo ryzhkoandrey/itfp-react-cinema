6b6829d4d23a98c70bc1e10967febb973c26c217
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-07-17 09:58:23 +0300

    16

---
+++ src/redux/slices/favoritesSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [];
+
+const favoritesSlice = createSlice({
+   name: 'favorite',
+   initialState,
+   reducers: {},
+});
+
+export default favoritesSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import movies from './slices/moviesSlice';
+import favoritesSlice from './slices/favoritesSlice';
 
 const store = configureStore({
    reducer: {
       movies,
+      favoritesSlice,
    },
 });
 
