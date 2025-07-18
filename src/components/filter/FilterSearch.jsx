2e36823a25583b8a7aa74e48ba78e8014088cfac
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-07-18 13:01:34 +0300

    17

--- src/components/filter/FilterSearch.jsx
+++ src/components/filter/FilterSearch.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { filterMovies } from '../../redux/slices/moviesSlice';
+
 function FilterSearch() {
+   const dispatch = useDispatch();
+   const [searchTerm, setSearchTerm] = useState('');
+
+   useEffect(() => {
+      const handler = setTimeout(() => {
+         dispatch(filterMovies(searchTerm));
+      }, 500);
+      return () => clearTimeout(handler);
+   }, [searchTerm, dispatch]);
+
    return (
       <div className="filter__search">
-         <input type="text" placeholder="Search..." />
+         <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search..."
+         />
          <button>
             <i className="fa fa-search"></i>
          </button>
