e6158a70b13de924225e9b0a3a8a6b124ed2b76a
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-07-19 12:44:25 +0300

    18

--- src/components/filter/FilterButtons.jsx
+++ src/components/filter/FilterButtons.jsx
@@ -3,10 +3,16 @@ import { useSelector } from 'react-redux';
 
 function FilterButtons() {
    const { filmsCategory } = useSelector((store) => store.movies);
-   const [active, changeActive] = useState('Movies');
+   const [active, changeActive] = useState('All');
 
    return (
       <div className="filter__buttons">
+         <button
+            onClick={() => changeActive('All')}
+            className={active === 'All' ? 'active' : null}
+         >
+            All
+         </button>
          {filmsCategory.map((value, index) => (
             <button
                onClick={() => changeActive(value)}
